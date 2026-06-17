d73cbb64946fc5b7e2a32fe807302bf6052d0654
FDStand-2 <fedor@durnov.com>
2026-06-17 10:51:58 +0200

    ETH and ThreadX

---
+++ Appli/Core/Src/eth.c
@@ -0,0 +1,210 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    eth.c
+  * @brief   This file provides code for the configuration
+  *          of the ETH instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "eth.h"
+#include "string.h"
+
+#if defined ( __ICCARM__ ) /*!< IAR Compiler */
+
+#pragma location=0x34100000
+ETH_DMADescTypeDef  DMARxDscrTab[ETH_DMA_RX_CH_CNT][ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
+#pragma location=0x341000C0
+ETH_DMADescTypeDef  DMATxDscrTab[ETH_DMA_TX_CH_CNT][ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
+
+#elif defined ( __CC_ARM )  /* MDK ARM Compiler */
+
+__attribute__((at(0x34100000))) ETH_DMADescTypeDef  DMARxDscrTab[ETH_DMA_RX_CH_CNT][ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
+__attribute__((at(0x341000C0))) ETH_DMADescTypeDef  DMATxDscrTab[ETH_DMA_TX_CH_CNT][ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
+
+#elif defined ( __GNUC__ ) /* GNU Compiler */
+
+ETH_DMADescTypeDef DMARxDscrTab[ETH_DMA_RX_CH_CNT][ETH_RX_DESC_CNT] __attribute__((section(".RxDecripSection"))); /* Ethernet Rx DMA Descriptors */
+ETH_DMADescTypeDef DMATxDscrTab[ETH_DMA_TX_CH_CNT][ETH_TX_DESC_CNT] __attribute__((section(".TxDecripSection")));   /* Ethernet Tx DMA Descriptors */
+
+#endif
+ETH_BufferTypeDef Txbuffer[ETH_TX_DESC_CNT * 2U];
+ETH_TxPacketConfig TxConfig;
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+ETH_HandleTypeDef heth1;
+
+/* ETH1 init function */
+void MX_ETH1_Init(void)
+{
+
+  /* USER CODE BEGIN ETH1_Init 0 */
+
+  /* USER CODE END ETH1_Init 0 */
+
+   static uint8_t MACAddr[6];
+
+  /* USER CODE BEGIN ETH1_Init 1 */
+
+  /* USER CODE END ETH1_Init 1 */
+
+  memset(&TxConfig, 0 , sizeof(ETH_TxPacketConfig));
+  TxConfig.Attributes = ETH_TX_PACKETS_FEATURES_CSUM | ETH_TX_PACKETS_FEATURES_CRCPAD;
+  TxConfig.ChecksumCtrl = ETH_CHECKSUM_IPHDR_PAYLOAD_INSERT_PHDR_CALC;
+  TxConfig.CRCPadCtrl = ETH_CRC_PAD_INSERT;
+  heth1.Instance = ETH1;
+  MACAddr[0] = 0x00;
+  MACAddr[1] = 0x80;
+  MACAddr[2] = 0xE1;
+  MACAddr[3] = 0x00;
+  MACAddr[4] = 0x00;
+  MACAddr[5] = 0x00;
+  heth1.Init.MACAddr = &MACAddr[0];
+  heth1.Init.MediaInterface = HAL_ETH_RMII_MODE;
+  for (int ch = 0; ch < ETH_DMA_CH_CNT; ch++)
+  {
+    heth1.Init.TxDesc[ch] = DMATxDscrTab[ch];
+    heth1.Init.RxDesc[ch] = DMARxDscrTab[ch];
+  }
+  heth1.Init.RxBuffLen = 1536;
+
+  /* USER CODE BEGIN MACADDRESS */
+
+  /* USER CODE END MACADDRESS */
+
+  if (HAL_ETH_Init(&heth1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ETH1_Init 2 */
+
+  /* USER CODE END ETH1_Init 2 */
+
+}
+
+void HAL_ETH_MspInit(ETH_HandleTypeDef* ethHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  if(ethHandle->Instance==ETH1)
+  {
+  /* USER CODE BEGIN ETH1_MspInit 0 */
+
+  /* USER CODE END ETH1_MspInit 0 */
+
+  /** Initializes the peripherals clock
+  */
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_ETH1;
+    PeriphClkInitStruct.Eth1ClockSelection = RCC_ETH1CLKSOURCE_HCLK;
+
+  /* USER CODE BEGIN MACADDRESS */
+
+  /* USER CODE END MACADDRESS */
+
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* ETH1 clock enable */
+    __HAL_RCC_ETH1_CLK_ENABLE();
+    __HAL_RCC_ETH1MAC_CLK_ENABLE();
+    __HAL_RCC_ETH1TX_CLK_ENABLE();
+    __HAL_RCC_ETH1RX_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    /**ETH1 GPIO Configuration
+    PD1     ------> ETH1_MDC
+    PD12     ------> ETH1_MDIO
+    PF10     ------> ETH1_RMII_CRS_DV
+    PF7     ------> ETH1_RMII_REF_CLK
+    PF5     ------> ETH1_CLK
+    PF15     ------> ETH1_RMII_RXD1
+    PF14     ------> ETH1_RMII_RXD0
+    PF11     ------> ETH1_RMII_TX_EN
+    PF13     ------> ETH1_RMII_TXD1
+    PF12     ------> ETH1_RMII_TXD0
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_7|GPIO_PIN_5|GPIO_PIN_15
+                          |GPIO_PIN_14|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH1;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    /* ETH1 interrupt Init */
+    HAL_NVIC_SetPriority(ETH1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(ETH1_IRQn);
+  /* USER CODE BEGIN ETH1_MspInit 1 */
+
+  /* USER CODE END ETH1_MspInit 1 */
+  }
+}
+
+void HAL_ETH_MspDeInit(ETH_HandleTypeDef* ethHandle)
+{
+
+  if(ethHandle->Instance==ETH1)
+  {
+  /* USER CODE BEGIN ETH1_MspDeInit 0 */
+
+  /* USER CODE END ETH1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ETH1_CLK_DISABLE();
+    __HAL_RCC_ETH1MAC_CLK_DISABLE();
+    __HAL_RCC_ETH1TX_CLK_DISABLE();
+    __HAL_RCC_ETH1RX_CLK_DISABLE();
+
+    /**ETH1 GPIO Configuration
+    PD1     ------> ETH1_MDC
+    PD12     ------> ETH1_MDIO
+    PF10     ------> ETH1_RMII_CRS_DV
+    PF7     ------> ETH1_RMII_REF_CLK
+    PF5     ------> ETH1_CLK
+    PF15     ------> ETH1_RMII_RXD1
+    PF14     ------> ETH1_RMII_RXD0
+    PF11     ------> ETH1_RMII_TX_EN
+    PF13     ------> ETH1_RMII_TXD1
+    PF12     ------> ETH1_RMII_TXD0
+    */
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_1|GPIO_PIN_12);
+
+    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_10|GPIO_PIN_7|GPIO_PIN_5|GPIO_PIN_15
+                          |GPIO_PIN_14|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_12);
+
+    /* ETH1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(ETH1_IRQn);
+  /* USER CODE BEGIN ETH1_MspDeInit 1 */
+
+  /* USER CODE END ETH1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
